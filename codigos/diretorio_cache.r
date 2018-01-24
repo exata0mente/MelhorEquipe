c792fcc281d5b0da046ecce4f22563dd42a46683
Exata0mente <exata0mente@gmail.com>
2018-01-24 19:56:39 -0200

    resolvido o problema de "warning" de pasta já existente.

--- codigos/diretorio_cache.r
+++ codigos/diretorio_cache.r
@@ -1,10 +1,13 @@
 diretorio_cache <- function(dir = NULL){
 # Este objeto função conterá os métodos para validação da existência da pasta cache e criação se necessário.
   
+  if(file.exists(paste0(getwd(),"/","data/jsonR/")))
+    dir <- paste0(getwd(),"/","data/jsonR/")
+  
   obter_diretorio <- function() dir
   # Método para obtenção do diretório que receberá a pasta cache
   
-  definir_diretorio <- function(novoDir=NULL){
+  definir_diretorio <- function(novoDir=dir){
   # Método para definição do diretório de trabalho que receberá os dados JSON
     setwd(novoDir)
     dir <<- novoDir
@@ -22,3 +25,4 @@ diretorio_cache <- function(dir = NULL){
   )
   
 }
+
