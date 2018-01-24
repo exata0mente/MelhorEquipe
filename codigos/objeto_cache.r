eab9ed46098b1477a1ced24e1314311729898d0a
Exata0mente <exata0mente@gmail.com>
2018-01-24 19:59:22 -0200

    melhorado a assimilação dos valores carregados/salvos

--- codigos/objeto_cache.r
+++ codigos/objeto_cache.r
@@ -1,28 +1,27 @@
 objeto_cache <- function(obj = NULL, dir=NULL, urlJSON=NULL){
 
+  ambiente <- globalenv()
   nomeObjeto <- obj
   obj <- NULL
-    
+  
   obter_objeto <- function() obj
   
   definir_objeto <- function(novObj=nomeObjeto, novoDir=dir){
-    objExis <- NULL
-    if(is.null(novObj) || is.null(novoDir))
-    #Se passar NULL como parâmetro, o objeto é "limpado"
-      return(NULL)
+    dir <<- novoDir
+    obj <<- novObj
     if(file.exists(paste0(novoDir,novObj,".RData"))){
-      message("Cache encontrado. Recuperando objeto")
-      objExis <- load(paste0(novoDir,novObj,".RData"))
-      obj <<- objExis
+      load(paste0(novoDir,novObj,".RData"), envir = ambiente)
+      obj <<- objJSON
     }
-    objExis
+    else
+      return(NULL)    
   }
-  
-  criar_objeto <- function(dir) {
+  criar_objeto <- function(novoDir=dir){
     require(jsonlite)
     objJSON <- fromJSON(urlJSON)
     obj <<- objJSON
-    save(file=paste0(dir, nomeObjeto, ".RData"))
+    assign("objJSON", objJSON, envir=ambiente)
+    save(objJSON, file=paste0(dir, nomeObjeto, ".RData"))
     rm(objJSON)
   }
 
@@ -33,3 +32,4 @@ objeto_cache <- function(obj = NULL, dir=NULL, urlJSON=NULL){
   )
   
 }
+
